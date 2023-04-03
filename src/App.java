4f05441d70f81ec8dd5d597a4229c87508dc3de9
aSchemitt <101745591+aSchemitt@users.noreply.github.com>
2023-04-03 14:20:20 -0300

    commit inicial

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // criar uma conexão http e salvar numa string
+
+        // extrair os dados que interessam
+
+        // manipular e exibir os dados
+    }
+}
